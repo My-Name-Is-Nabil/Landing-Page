db944a6f270706de104ca47508fe90a6a4f7bf14
My-Name-Is-Nabil <midorady9999@gmail.com>
2021-03-23 18:35:21 +0200

    First Commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Landing Page Project
+## Table Of Contents 
+- Building Navigation Bar
+- Distinguish Section in View
+- Scroll To Section
+- Scroll To Top Button
+
+## Building Navigation Bar
+
+The function "createNavBar()" is a self-invoking function that builds a navigation bar using a document fragment. First, it creates "li" items as much as there are sections and add an appropriate class for them to style them, then it appends each item to the document fragment. After that, the fragment is appended to "ul" element of the navigation bar.
+
+The navigation bar disappears after two seconds using "setInterval" function, but if the user scrolls or hovers over the bottom of the page the navigation will reappear and the timer will be reset.
+
+## Distinguish Section in View
+
+The section in view will have a distinct class called "active-section", this class will be added by the function "highlightSection()", in this functionw will also add the "active-section-nav" class to highlight the section in the navigation bar. The "active-section" class will give the section a black background and the "active-section-nav" class will give the active section nav item a yellow color.
+
+## Scroll To Section
+
+Clicking the section nav item will call the function "scrollToSection()", which will scroll
+the browser to the required section, the scrolling will be smooth due to the css property "scroll-behaviour".
+
+## Scroll To Top Button
+
+If the browser is scrolled bellof the fold of the page, a scroll to top button will appear at the bottom right of the screen, this is achieved by "showScrollToTopButton()" function.
+Clicking on the button will scroll the user to the top of the page using the function "scrollToTop()".
---
+++ js/app.js
@@ -0,0 +1,118 @@
+// Variables
+
+const sections = document.querySelectorAll('section');
+const numberOfSections = sections.length;
+const nav = document.querySelector('#navbar__list');
+const scrollToTopButton = document.querySelector('.scroll-up');
+// Hide scroll bar every 2 seconds
+var hideNavBarTimer = setInterval(hideNavBar,2000);
+
+
+//Function
+
+
+// Self invoking function to create NavBar
+(function createNavBar(){  
+    const fragment = document.createDocumentFragment();
+    for(let i=0;i<numberOfSections;i++){
+        const li = document.createElement('li');
+        li.classList.add('navbar__list__item');
+        li.textContent=`Section ${i+1}`;
+        fragment.appendChild(li);
+    }
+    nav.appendChild(fragment);
+}());
+
+function scrollToSection(event){
+    // Event delegation 
+    if (event.target.nodeName==='LI'){
+        for(let i=0; i<numberOfSections; i++){
+            if(nav.children[i]===event.target){
+                //Scroll To Desired Section
+                let positionX = sections[i].offsetLeft;        
+                let positionY = sections[i].offsetTop;          
+                window.scrollTo(positionX, positionY);    
+            }
+        }
+    }
+}
+
+function highlightSection(){
+    // Reset timer 
+    clearInterval(hideNavBarTimer);
+    const footer = document.querySelector('.page__footer');
+    footer.classList.remove('hide');
+    hideNavBarTimer=setInterval(hideNavBar,2000);
+    const activeSection = document.querySelector('.active-section');
+    const activeSectionNav = document.querySelector('.active-section-nav');
+    for(let i=1;i<numberOfSections;i++){
+        // If scroll position is at the center of the screen
+        if (window.scrollY<sections[i].offsetTop-window.innerHeight/2){
+            // Check if there is a transition between section 
+            if (sections[i-1]!==activeSection){
+                sections[i-1].classList.add('active-section');
+                nav.children[i-1].classList.add('active-section-nav');
+                if(activeSection)
+                    activeSection.classList.remove('active-section');
+                if(activeSectionNav)
+                    activeSectionNav.classList.remove('active-section-nav');
+            }
+            return;
+        }
+    }
+    if (sections[numberOfSections-1]!==activeSection){
+        sections[numberOfSections-1].classList.add('active-section');
+        nav.children[numberOfSections-1].classList.add('active-section-nav');
+        if(activeSection)
+            activeSection.classList.remove('active-section');
+        if(activeSectionNav)
+            activeSectionNav.classList.remove('active-section-nav');
+    }
+}
+
+function showscrollToTopButton(){
+    // Check if scroll position is after the fold of the page
+    if (window.scrollY>=window.innerHeight){
+        scrollToTopButton.style.opacity=1;
+        scrollToTopButton.style.visibility='visible';
+    }
+    else{
+        scrollToTopButton.style.opacity=0;
+        scrollToTopButton.style.visibility='hidden';
+    }
+}
+
+function scrollToTop(){
+    const positionX = window.scrollX;
+    window.scroll(positionX,0);
+}
+
+function hideNavBar(){
+    const footer = document.querySelector('.page__footer');
+    footer.classList.add('hide');
+}
+
+
+//Events
+
+
+nav.addEventListener('click',scrollToSection);
+document.addEventListener('scroll',function(){
+    highlightSection();
+    showscrollToTopButton();
+});
+document.addEventListener('DOMContentLoaded',function(){
+    highlightSection();
+    showscrollToTopButton();
+});
+scrollToTopButton.addEventListener('click',scrollToTop);
+document.addEventListener('mousemove',function(event){
+    if (window.outerHeight-event.screenY<50){
+        clearInterval(hideNavBarTimer);
+        hideNavBarTimer=setInterval(hideNavBar,2000);
+        const footer = document.querySelector('.page__footer');
+        footer.classList.remove('hide');
+    }
+})
+
+showscrollToTopButton();
